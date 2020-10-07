737901c83a9ced0778125cb3c1a6172d3a1b53c4
Derek <dbacy@my.edgetechacademy.edu>
2020-10-07 03:40:14 -0500

    Add project files.

---
+++ HandsOn/ArrayMax.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HandsOn
+{
+    class ArrayMax
+    {
+        int[] array1 = { 34, 26, 38, 92, 187 };
+      
+
+    }
+}
---
+++ HandsOn/MultiplicationTable.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HandsOn
+{
+    class MultiplicationTable
+    {
+        static void Main(string[] args)
+        {
+            /* int sizeOfGrid;
+            sizeOfGrid = Utils.GetNumber("Enter number: ");
+            for (int header = 0; header<= sizeOfGrid; header++)
+            {
+                if (header == 0)
+                {
+                    Console.WriteLine("x\t|\t");
+                } 
+                else
+                {
+                    Console.WriteLine(header + "\t");
+                } 
+            }
+            Console.WriteLine("\n--------------------------------");
+            for (int x = 1; x <= sizeOfGrid; x++)
+            {
+                Console.WriteLine(x + "\t|\t");
+                for (int y = 1; y<=sizeOfGrid; y++)
+                {
+                    Console.WriteLine(x * y + "\t");
+                }
+                Console.WriteLine("\n");
+
+            } */
+
+
+           
+            
+            
+            
+            
+            
+            /*int j = Utils.GetNumber("Enter A Number: ");
+            for (int i = 0; i <= 300; i++)
+            {
+                Console.WriteLine("{1} X {0} = {2}", i, j, i * j);
+                Console.ReadLine();
+            } */
+
+           
+            
+            
+            
+            
+            
+            
+            
+            int j, n;
+
+            n = Utils.GetNumber("Enter Number: ");
+            Console.Write("\n");
+            for (j = 1; j <= 40; j++)
+            {
+                Console.Write("{0} X {1} = {2} \n", n, j, n * j);
+            }
+        }
+
+    }
+}
